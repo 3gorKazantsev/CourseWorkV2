5c0792357f0468f32f41e9eb476803f24f8e9659
Egor Kazantsev <3gork4z4ncev@gmail.com>
2021-05-11 00:05:14 +0500

    added the FAB (without a ClickListener) on the TableFragment and hide it while scrolling the RecyclerView down

--- app/src/main/java/org/kazantsev/coursework/fragments/TableFragment.kt
+++ app/src/main/java/org/kazantsev/coursework/fragments/TableFragment.kt
@@ -5,20 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import org.kazantsev.coursework.R
 import org.kazantsev.coursework.adapters.*
 import org.kazantsev.coursework.data.Table
 import org.kazantsev.coursework.data.TempData
 import org.kazantsev.coursework.databinding.FragmentTableBinding
-import org.kazantsev.coursework.viewmodels.TablesViewModel
+import kotlin.math.abs
 
 class TableFragment : Fragment() {
 
-    // View binding
+    // View Binding
     private var _binding: FragmentTableBinding? = null
     private val binding get() = _binding!!
 
@@ -30,14 +27,21 @@ class TableFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        // Init View binding
+        // init View Binding
         _binding = FragmentTableBinding.inflate(inflater, container, false)
-        // Inflate the layout for this fragment
+        // inflate the layout for this fragment
         val view = binding.root
 
         // setting RecyclerView
         binding.recyclerView.layoutManager = LinearLayoutManager(context)
         setAdapter(args.selectedTable)
+        // hide the FAB on scroll down
+        hideFabOnScrollDown()
+
+        // FAB click listener
+        binding.fab.setOnClickListener {
+
+        }
 
         return view
     }
@@ -47,6 +51,7 @@ class TableFragment : Fragment() {
         _binding = null
     }
 
+    // function for choosing the adapter depending on the safe args
     private fun setAdapter(selectedTable: Table) {
         binding.recyclerView.adapter = when (selectedTable) {
             Table.CLIENTS_TABLE -> ClientAdapter(TempData.getClientList())
@@ -55,6 +60,16 @@ class TableFragment : Fragment() {
             Table.ORDERS_TABLE -> OrderAdapter(TempData.getOrderList())
             Table.PRODUCTS_TABLE -> ProductAdapter(TempData.getProductList())
             Table.SUPPLIERS_TABLE -> SupplierAdapter(TempData.getSupplierList())
+        }
+    }
+
+    // function for hiding the FAB while scrolling the RecyclerView down
+    private fun hideFabOnScrollDown() {
+        binding.recyclerView.setOnScrollChangeListener { _, _, _, _, oldY ->
+            when {
+                oldY < 0 -> binding.fab.hide()
+                else -> binding.fab.show()
+            }
         }
     }
 }
